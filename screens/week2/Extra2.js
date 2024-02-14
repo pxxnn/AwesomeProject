314fe1711ad4484a3f389c59743326ed839ab822
pxxnn <95416192+pxxnn@users.noreply.github.com>
2024-02-14 16:18:50 +0700

    Initial commit

---
+++ screens/week2/Extra2.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function Extra3() {
+  return (
+    <View style={{ flex : 1, flexDirection: 'row'}}>
+      <View style={{ flex : 1, backgroundColor : '#FFEBB6' }}></View>
+      <View style={{ flex : 2, flexDirection: 'column'}}>
+      <View style={{ flex : 1, backgroundColor : '#8BD7B1' }}></View>
+      <View style={{ flex : 2, flexDirection: 'row'}}>
+      <View style={{ flex : 1, backgroundColor : '#FE706E' }}></View> 
+      <View style={{ flex : 1, flexDirection: 'column'}}>
+      <View style={{ flex : 1, backgroundColor : '#FFCB65' }}></View>
+      <View style={{ flex : 1, backgroundColor : '#FE706E' }}></View>
+      </View>
+      </View>
+      </View>
+    </View>
+
+  );
+
+}
